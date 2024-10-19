376310e51dfa55438fb6ac27b10773c856f53c45
Arthur <arthurramos7890@gmail.com>
2024-10-19 16:30:00 -0300

    1071 done

--- 1071/1071.c
+++ 1071/1071.c
@@ -2,24 +2,21 @@
 #include <stdlib.h>
 
 int main(){
-    int n1, n2, imps = 0;
+    int n1, n2, imps = 0, aux;
 
     scanf("%d %d", &n1, &n2);
 
-    if(n1 >= n2){
-        for(int c = n1-1; c > n2; c--){
-            if(c % 2 != 0){
-                imps += c;
-            }
-        }
-    }else{
-        for(int c = n1+1; c > n2-1; c++){
-            if(c % 2 != 0){
-                imps += c;
-            }
-        }
+    if(n1 > n2){
+        aux = n1;
+        n1 = n2;
+        n2 = aux;
     }
+
+    for(int c = n1+1; c < n2; c++)
+        if(c % 2)
+            imps += c;
 
     printf("%d\n", imps);
 
+    return 0;
 }
